db8e9484faeec7c8156f21572b62e7e26d761103
iceseyes <bianchi.massimo@gmail.com>
2017-09-02 15:34:13 +0200

    Snippets for namespace and class definition

--- setup_snippets.el
+++ setup_snippets.el
@@ -7,8 +7,9 @@
 (setq yas-snippet-dirs (append yas-snippet-dirs
                                '("~/.emacs.d/emacs-config/snippets/")))
 
+(yas-reload-all)
 (auto-insert-mode)
-(setq auto-insert-directory "~/.emacs.d/emacs-config/snippets/templates/")
 
+(setq auto-insert-directory "~/.emacs.d/emacs-config/snippets/templates/")
 (define-auto-insert "\\.\\([Hh]\\|hh\\|hpp\\)\\'" ["cpp-template.hpp" autoinsert-yas-expand])
 (define-auto-insert "\\.\\([Cc]\\|cc\\|cpp\\)\\'" ["cpp-template.cpp" autoinsert-yas-expand])
